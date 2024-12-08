8a765cf90a2e5632128fdaeaed368a8995bc38c8
BugHunter-X <askzenai@gmail.com>
2024-12-08 12:52:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,9 @@
+function foo(a, b) {
+  if (a == null || b == null) {
+    return 0; //this return statement is causing the bug
+  }
+  return a + b;
+}
+console.log(foo(null, 5)); // Output: 0
+console.log(foo(5, null)); // Output: 0
+console.log(foo(5, 5)); // Output: 10
